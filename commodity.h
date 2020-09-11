a334c7ac0cee1429ed21b1676eb341ea21a18b30
Mneverever <54008162+Mneverever@users.noreply.github.com>
2020-09-11 15:28:16 +0800

    Create commodity.h

---
+++ commodity.h
@@ -0,0 +1,12 @@
+#ifndef COMMODITY_H
+#define COMMODITY_H
+class commodity{
+        public:
+                commodity(int , int );
+                int getprice();
+                int getoccupy();
+        private:
+                int price;
+                int occupy;
+};
+#endif // COMMODITY_H
